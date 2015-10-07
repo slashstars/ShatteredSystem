d1b60923dbb5a416bdef14bb80e0057fdf47328a
Stoil <stoilstoychev@gmail.com>
2015-10-08 00:55:47 +0200

    Initial commit. Added Shatter script that allows a sphere to be replaced by subspheres of (roughly) equal volume. Added Utility script.

---
+++ Assets/Script/Shatter.cs
@@ -0,0 +1,140 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.Script;
+
+public class Shatter : MonoBehaviour
+{
+    public float shatterRatio;
+    public GameObject subPiece;
+    private Transform piecesContainer;
+    private Transform piecesGlobal;
+    private readonly string PIECES_GLOBAL = "PiecesGlobal";
+
+    // Use this for initialization
+    void Start()
+    {
+        piecesGlobal = GameObject.Find(PIECES_GLOBAL).transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnMouseDown()
+    {
+        ShatterFromCenter(1);
+    }
+
+    /// <summary>
+    /// Radius of the sphere object.
+    /// </summary>
+    private float Radius
+    {
+        get
+        {
+            return GetComponent<SphereCollider>().radius * transform.localScale.x;
+        }
+    }
+
+    /// <summary>
+    /// Calculated radius of subspheres afer current sphere is "shattered".
+    /// </summary>
+    private float NewPieceRadius
+    {
+        get
+        {
+            return Radius * shatterRatio;
+        }
+    }
+
+    /// <summary>
+    /// Replaces current sphere with subspheres.
+    /// Subspheres are forced outward from current sphere's center.
+    /// </summary>
+    /// <param name="shatterStrength">Magnitude of force scattering the subsphere.</param>
+    private void ShatterFromCenter(int shatterStrength)
+    {
+        CreateSubPieces();
+
+        var currentVelocity = GetComponent<Rigidbody>().velocity;
+        foreach (var piece in piecesContainer.GetComponentsInChildren<Rigidbody>())
+        {
+            piece.AddForce((piece.transform.position - transform.position) * shatterStrength + currentVelocity, ForceMode.Impulse);
+        }
+
+        Destroy(gameObject);
+    }
+
+    /// <summary>
+    /// Creates the subspheres intended to replace current sphere.
+    /// The number of subspheres is determined by the public shatterRatio variable.
+    /// </summary>
+    private void CreateSubPieces()
+    {
+        piecesContainer = new GameObject().transform;
+        piecesContainer.parent = piecesGlobal;
+
+        var numberOfCirlceLayers = Mathf.RoundToInt(Radius / NewPieceRadius / 2);
+        addBallsInLayersRecursive(numberOfCirlceLayers, Radius - NewPieceRadius);
+    }
+
+    /// <summary>
+    /// Adds subspheres evenly spaced in a circle around the current center.
+    /// Recursive function.
+    /// </summary>
+    /// <param name="layerIndex">The current circle layer we're adding spheres to.</param>
+    /// <param name="layerRadius">The radius of the current circle layer.</param>
+    private void addBallsInLayersRecursive(int layerIndex, float layerRadius)
+    {
+        var numberOfBallsInLayer = HowManyBallsInLayer(layerRadius, NewPieceRadius);
+
+        addBallsInCurrentLayer(layerRadius, numberOfBallsInLayer);
+
+        if (layerIndex > 1)
+            addBallsInLayersRecursive(--layerIndex, layerRadius - NewPieceRadius * 2);
+    }
+
+    /// <summary>
+    /// Adds spheres evently spaced in a circle shape around the current position of object.
+    /// </summary>
+    /// <param name="radius">The radius of the circle.</param>
+    /// <param name="numberOfBalls">Number of spheres to add.</param>
+    private void addBallsInCurrentLayer(float radius, int numberOfBalls)
+    {
+        var offset = new Vector3(radius, 0, 0);
+        var pointsInLayers = Utility.DefineEvenlySpacedPointsAlongACircle(radius, numberOfBalls);
+
+        foreach (var point in pointsInLayers)
+        {
+            var newPiece = InstantiateScaledBall();
+            newPiece.transform.position = transform.position + point;
+            newPiece.transform.parent = piecesContainer;
+        }
+    }
+
+    /// <summary>
+    /// Creates a new instances of a sphere and scales it to the current shatter ratio.
+    /// </summary>
+    /// <returns>The newly created sphere.</returns>
+    private GameObject InstantiateScaledBall()
+    {
+        var newPiece = Instantiate(subPiece);
+        newPiece.transform.localScale = transform.localScale * shatterRatio;
+        return newPiece;
+    }
+
+    /// <summary>
+    /// Calculates the number of subspheres that would fit in a circle layer given the subsphere size.
+    /// </summary>
+    /// <param name="radiusOfLayer">The radius of the circle.</param>
+    /// <param name="newBallRadius">The radius of the subspheres.</param>
+    /// <returns></returns>
+    private int HowManyBallsInLayer(float radiusOfLayer, float newBallRadius)
+    {
+        var distanceBetweenTwoBalls = newBallRadius * 2;
+        var angleBetweenBalls = Utility.GetAngleBInTriangleViaThreeSides(radiusOfLayer, distanceBetweenTwoBalls, radiusOfLayer);
+        return Mathf.RoundToInt(360 / angleBetweenBalls);
+    }
+}
---
+++ Assets/Script/Utility.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Script
+{
+    public static class Utility
+    {
+        /// <summary>
+        /// Calculates the B angle in an ABC triangle by using the length of its sides.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        /// <returns>The angle in degrees.</returns>
+        public static float GetAngleBInTriangleViaThreeSides(float a, float b, float c)
+        {
+            var aSquared = Mathf.Pow(a, 2);
+            var bSquared = Mathf.Pow(b, 2);
+            var cSquared = Mathf.Pow(c, 2);
+
+            var divisor = 2 * a * c;
+            var dividible = cSquared + aSquared - bSquared;
+
+            return Mathf.Acos(dividible / divisor) * Mathf.Rad2Deg;
+        }
+
+        /// <summary>
+        /// Calculates the coordinates for evenly spaced points along a circle.
+        /// </summary>
+        /// <param name="radius">The radius of the circe.</param>
+        /// <param name="numberOfPoints">How many points we're going to add to the circle.</param>
+        /// <returns>A list of the point coordinates (Vector3 with z==0)</returns>
+        public static List<Vector3> DefineEvenlySpacedPointsAlongACircle(float radius, int numberOfPoints)
+        {
+            List<Vector3> coordinates = new List<Vector3>();
+
+            for (var i = 1; i <= numberOfPoints; i++)
+            {
+                var theta = ((Mathf.PI * 2) / numberOfPoints);
+                var angle = (theta * i);
+
+                var ballX = (radius * Mathf.Cos(angle));
+                var ballY = (radius * Mathf.Sin(angle));
+
+                coordinates.Add(new Vector3(ballX, ballY, 0));
+            }
+
+            return coordinates;
+        }
+    }
+}
